c26836eff9332a8964886b03480ea9f587957940
Ntlele <ntleledavid07@gmail.com>
2024-07-23 21:10:43 +0200

    FIXED ERROR

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,5 @@
 export default function getListStudents() {
-
-    return [
+  return [
     { firstName: 'Guillaume', id: 1, location: 'San Francisco' },
     { firstName: 'James', id: 2, location: 'Columbia' },
     { firstName: 'Serena', id: 5, location: 'San Francisco' },

022ed7be5107aa17b4a0e6ca6e92c1113fee838e
Ntlele <ntleledavid07@gmail.com>
2024-07-23 20:58:14 +0200

    get student list done

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+export default function getListStudents(){
+
+    return [
+        {id:1, firstname: 'Guillaume', location: 'San Francisco'},
+        {id:2, firstname: 'James', location: 'Columbia'},
+        {id:5, firstname: 'Serena', location: 'San Francisco'}
+    ];
+}

aa1978fc8a733f36642447df1190724eb4e28021
Ntlele <ntleledavid07@gmail.com>
2024-07-24 20:15:51 +0200

    map the student's Ids

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,7 @@
+export default function getStudentsIdSum(studentL) {
+  if (!Array.isArray(studentL)) {
+    return 0;
+  }
+
+  return studentL.reduce((sum, student) => sum + student.id, 0)
+}
--- ES6_data_manipulation/main.js
+++ ES6_data_manipulation/main.js
@@ -1,6 +1,8 @@
 import getListStudents from "./0-get_list_students";
 import getListStudentIds from "./1-get_list_student_ids";
 import getStudentsByLocation from "./2-get_students_by_loc";
+import getStudentsIdSum from "./3-get_ids_sum";
 
 console.log(getListStudentIds(getListStudents()));
 console.log(getStudentsByLocation(getListStudents(), 'San Francisco'));
+console.log(getStudentsIdSum(getListStudents()));

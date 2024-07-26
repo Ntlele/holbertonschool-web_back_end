615e4c053463d7d7889c214ebf225f993d100df0
Ntlele <ntleledavid07@gmail.com>
2024-07-26 21:35:53 +0200

    Error fix

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -4,7 +4,7 @@ export default function updateUniqueItems(map) {
   }
 
   for (const [key, value] of map.entries()) {
-    if (value ===1) {
+    if (value === 1) {
       map.set(key, 100);
     }
   }

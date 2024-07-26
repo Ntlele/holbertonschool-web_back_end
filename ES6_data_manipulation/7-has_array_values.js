cab561aa5a9c793b50d39c25bb9766b7f1c3af93
Ntlele <ntleledavid07@gmail.com>
2024-07-26 21:32:19 +0200

    Error fix

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,7 +1,7 @@
 export default function hasValuesFromArray(set, array) {
   for (const value of array) {
     if (!set.has(value)) {
-        return false;
+      return false;
     }
   }
   return true;
--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 export default function cleanSet(set, startString) {
   const result = [];
 
-  if (!startString) {
+  if (typeof startString !== 'string' || startString === '') {
     return '';
   }
 
   for (const value of set) {
-    if (value.startsWith(startString)) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
       result.push(value.slice(startString.length));
     }
   }

94a011f01ffef55488761a8f16e78d52c5894bd8
Ntlele <ntleledavid07@gmail.com>
2024-07-26 20:29:45 +0200

    edited the main file

--- ES6_data_manipulation/main.js
+++ ES6_data_manipulation/main.js
@@ -2,7 +2,10 @@ import getListStudents from "./0-get_list_students";
 import getListStudentIds from "./1-get_list_student_ids";
 import getStudentsByLocation from "./2-get_students_by_loc";
 import getStudentsIdSum from "./3-get_ids_sum";
+import createInt8TypedArray from "./5-typed_arrays";
 
 console.log(getListStudentIds(getListStudents()));
 console.log(getStudentsByLocation(getListStudents(), 'San Francisco'));
 console.log(getStudentsIdSum(getListStudents()));
+console.log("\n");
+console.log(createInt8TypedArray(10, 2, 89));

bbf17abcf3962b3edd224c71e316685dcaadb004
Ntlele <ntleledavid07@gmail.com>
2024-07-26 20:45:59 +0200

    from Array to Set

--- ES6_data_manipulation/main.js
+++ ES6_data_manipulation/main.js
@@ -3,9 +3,11 @@ import getListStudentIds from "./1-get_list_student_ids";
 import getStudentsByLocation from "./2-get_students_by_loc";
 import getStudentsIdSum from "./3-get_ids_sum";
 import createInt8TypedArray from "./5-typed_arrays";
+import setFromArray from "./6-set";
 
 console.log(getListStudentIds(getListStudents()));
 console.log(getStudentsByLocation(getListStudents(), 'San Francisco'));
 console.log(getStudentsIdSum(getListStudents()));
 console.log("\n");
 console.log(createInt8TypedArray(10, 2, 89));
+console.log(setFromArray([12, 32, 15, 78, 98, 15]));

0136548f6892d62047de53c8c8a52304fab7593f
Ntlele <ntleledavid07@gmail.com>
2024-07-28 13:55:19 +0200

    An empty promise

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const [photo, user] = results;
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}

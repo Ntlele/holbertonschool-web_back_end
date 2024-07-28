1daca96079fcfc8582fd12092725d34cd50a8010
Ntlele <ntleledavid07@gmail.com>
2024-07-28 14:16:09 +0200

    An empty promise

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,11 +1,17 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([photo, user]) => {
-      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-    })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+  const myPromise = Promise.all([uploadPhoto(), createUser()]).then(
+    ([result1, result2]) => {
+      const myBody = result1.body;
+      const myName = result2.firstName;
+      const myLast = result2.lastName;
+
+      console.log(`${myBody} ${myName} ${myLast}`);
+    },
+  ).catch(() => {
+    console.log('Signup system offline');
+  });
+
+  return myPromise;
 }

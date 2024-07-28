8a02f15b7fea1315a5ea4d1c5cd8f784bae1b06b
Ntlele <ntleledavid07@gmail.com>
2024-07-28 13:45:15 +0200

    An empty promise

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,4 +1,4 @@
-function getFullResponseFromAPI(success) {
+export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
       resolve({

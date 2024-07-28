a3c46d5f3106b159ee3bb92a713806e41f4215c4
Ntlele <ntleledavid07@gmail.com>
2024-07-28 14:53:09 +0200

    An empty promise

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -7,6 +7,6 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     uploadPhoto(fileName),
   ]).then((results) => results.map((result) => ({
     status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
+    value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
   })));
 }

a52dd149c6bbcb9e5f76b054a6d4949b927069ca
Ntlele <ntleledavid07@gmail.com>
2024-08-18 15:29:48 +0200

    annotation in tuples

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -5,7 +5,7 @@ Module 5-sum_list
 """
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ Returns the tuple of the arguments """
     return k, v
 

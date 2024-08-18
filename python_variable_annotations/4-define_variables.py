d94958b3d8aebb83777b364eaacabf2907c0be15
Ntlele <ntleledavid07@gmail.com>
2024-08-18 14:27:06 +0200

    Define and anotate variables

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -6,5 +6,5 @@ Module 4-define_variables
 
 a: int = 1
 pi: float = 3.14
-i_understand_annotations:bool = True
-school:str = "Holberton"
+i_understand_annotations: bool = True
+school: str = "Holberton"

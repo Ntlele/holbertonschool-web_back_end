f892dafcb68565828a94f42f46741f57ce89c072
Ntlele <ntleledavid07@gmail.com>
2024-08-18 14:33:02 +0200

    annotation in lists

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from typing import List
+"""
+Module 5-sum_list
+"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ Retuns the sum of listed float values """
+    return sum(input_list)

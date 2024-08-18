ae0a3b70ae888650ad6a131c493b6f1b9454f401
Ntlele <ntleledavid07@gmail.com>
2024-08-18 14:36:55 +0200

    annotation in lists

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -7,10 +7,6 @@ Module 5-sum_list
 
 def sum_list(input_list: List[float]) -> float:
     """
-    Calculate the sum of a list of floating-point numbers.
-    Args:
-        input_list (List[float]): A list of floating-point numbers.
-
     Returns:
         float: The sum of all the numbers in the input list.        
     """

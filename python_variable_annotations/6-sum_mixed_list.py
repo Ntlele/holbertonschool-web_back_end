4c735ecefc728697dda8e8f4375caf00a1f6aa71
Ntlele <ntleledavid07@gmail.com>
2024-08-18 14:56:47 +0200

    annotation in lists

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -5,6 +5,6 @@ Module 5-sum_list
 """
 
 
-def sum_mixed_list(mxd_lst: List[Union[int, float]]):
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """ Returns the sum of the listed integers and the floatsas a float value"""
     return sum(mxd_lst)

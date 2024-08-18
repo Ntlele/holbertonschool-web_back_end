39a97e8bac19159aeb05335efba59560bdefd3e9
Ntlele <ntleledavid07@gmail.com>
2024-08-18 16:30:36 +0200

    Fixid documentation

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-from typing import Tuple, Union
 """
 Module 5-sum_list
 """
+from typing import Tuple, Union
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:

ac8ae9d042b9cd144cd58885804ae57e4749a68c
Ntlele <ntleledavid07@gmail.com>
2024-08-18 16:28:24 +0200

    Fixid documentation

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 from typing import Iterable, List, Tuple, Sequence
 """
-Module 9-element_length function that gets the leghth of a list
+    Contains the element_length function
 """
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
-    function that gets the leghth of a list
-
-    Args:
-        lst (list): list of values
-    Returns:
-        list of tuples
+        Returns a list of tuples, where each
+        tuple contains an element from the
+        input sequence and the length of that element.
+        Args:
+            lst (Sequence[Any]): A sequence of elements
+            that can be passed to the
+            `len()` function (e.g., strings, lists, tuples).
+        Returns:
+            List[Tuple[Any, int]]: A list of tuples.
+            Each tuple contains an element
+            from the input sequence and the length of that
+            element.
     """
     return [(i, len(i)) for i in lst]

4f0e3634de9c73c1215f5c2a321d2f4af4e1236b
Ntlele <ntleledavid07@gmail.com>
2024-08-18 13:20:17 +0200

    Error handling

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -3,6 +3,7 @@
 Module 0-add with function that returns a sum of two floating-point numbers.
 """
 
+
 def add(a: float, b: float) -> float:
     """ returns the sum of two floating-point numbers """
     return a + b

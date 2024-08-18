bb2a35a9b32bb3cdecc123bb98e119036e1bf497
Ntlele <ntleledavid07@gmail.com>
2024-08-18 16:31:14 +0200

    Fixid documentation

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-from typing import List
 """
 Module 5-sum_list
 """
+from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-from typing import List, Union
 """
 Module 5-sum_list
 """
+from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:

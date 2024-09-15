003ac2ba7d7a239598bbb392354718f898a45920
Ntlele <ntleledavid07@gmail.com>
2024-09-15 13:12:00 +0200

    This is python pagination

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -48,7 +48,7 @@ class Server:
             self.__dataset = dataset[1:]
 
         return self.__dataset
-    
+
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Gets specific data

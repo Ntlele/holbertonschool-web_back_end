1a6e9fd99254183115c2af25654b1b5436707574
Ntlele <ntleledavid07@gmail.com>
2024-09-17 19:37:55 +0200

    A function that prints a string

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,3 +1,14 @@
+// 0-console.js
+
+/**
+ * Function that prints a message to the console
+ * @param {string} message - The message to print
+ */
 function displayMessage(STDOUT) {
-  console.log(STDOUT);
+    console.log(STDOUT);
 }
+
+// Export the function for use in tests
+module.exports = {
+    displayMessage
+};

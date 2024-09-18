2b75daec9be5878045d00448cacc34388ebb9436
Ntlele <ntleledavid07@gmail.com>
2024-09-18 07:45:52 +0200

    A function that reads a string from the user input

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -9,19 +9,19 @@ function countStudents(path) {
 
     lines.forEach(line => {
       const [firstName, field] = line.split(',');
-        if (firstName && field) { // Ensure that both values are present
-          if (!studentCount[field]) {
-            studentCount[field] = 0;
-            studentNames[field] = [];
-            }
-            studentCount[field]++;
-            studentNames[field].push(firstName);
-        }
+      if (firstName && field) { // Ensure that both values are present
+        if (!studentCount[field]) {
+          studentCount[field] = 0;
+          studentNames[field] = [];
+          }
+          studentCount[field]++;
+          studentNames[field].push(firstName);
+      }
     });
 
     console.log(`Number of students: ${lines.length}`);
     for (const field in studentCount) {
-        console.log(`Number of students in ${field}: ${studentCount[field]}. List: ${studentNames[field].join(', ')}`);
+      console.log(`Number of students in ${field}: ${studentCount[field]}. List: ${studentNames[field].join(', ')}`);
     }
 
   } catch (error) {

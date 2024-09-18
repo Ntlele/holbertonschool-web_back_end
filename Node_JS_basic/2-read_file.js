089629805e6fb24d5ea8832f1b1e4d103fd3c3a9
Ntlele <ntleledavid07@gmail.com>
2024-09-18 07:43:40 +0200

    A function that reads a string from the user input

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    const lines = data.trim().split('\n');
+    const studentCount = {};
+    const studentNames = {};
+
+    lines.forEach(line => {
+      const [firstName, field] = line.split(',');
+        if (firstName && field) { // Ensure that both values are present
+          if (!studentCount[field]) {
+            studentCount[field] = 0;
+            studentNames[field] = [];
+            }
+            studentCount[field]++;
+            studentNames[field].push(firstName);
+        }
+    });
+
+    console.log(`Number of students: ${lines.length}`);
+    for (const field in studentCount) {
+        console.log(`Number of students in ${field}: ${studentCount[field]}. List: ${studentNames[field].join(', ')}`);
+    }
+
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}

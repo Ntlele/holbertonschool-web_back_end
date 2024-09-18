378b0b0d77ff21c7ca7d9d22fac992aaa1ee5269
Ntlele <ntleledavid07@gmail.com>
2024-09-18 07:47:04 +0200

    A function that reads a string from the user input

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -2,29 +2,31 @@ const fs = require('fs');
 
 function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, 'utf-8');
-    const lines = data.trim().split('\n');
-    const studentCount = {};
-    const studentNames = {};
-
-    lines.forEach(line => {
-      const [firstName, field] = line.split(',');
-      if (firstName && field) { // Ensure that both values are present
-        if (!studentCount[field]) {
-          studentCount[field] = 0;
-          studentNames[field] = [];
-          }
-          studentCount[field]++;
-          studentNames[field].push(firstName);
-      }
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const students = lines.slice(1).map((line) => {
+      const [firstname, lastname, age, field] = line.split(',');
+      return {
+        firstname, lastname, age, field,
+      };
     });
 
-    console.log(`Number of students: ${lines.length}`);
-    for (const field in studentCount) {
-      console.log(`Number of students in ${field}: ${studentCount[field]}. List: ${studentNames[field].join(', ')}`);
-    }
+    console.log(`Number of students: ${students.length}`);
 
+    const fields = {};
+    students.forEach((student) => {
+      if (!fields[student.field]) {
+        fields[student.field] = [];
+      }
+      fields[student.field].push(student.firstname);
+    });
+
+    for (const [field, firstnames] of Object.entries(fields)) {
+      console.log(`Number of students in ${field}: ${firstnames.length}. List: ${firstnames.join(', ')}`);
+    }
   } catch (error) {
     throw new Error('Cannot load the database');
   }
 }
+
+module.exports = countStudents
